abedd878062722b2b5788dffb42f024e29e472ca
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2021-12-27 15:42:39 -0500

    Two Pointers LeetCode

---
+++ TwoPointers/SubarrayProductLessThanK.js
@@ -0,0 +1,57 @@
+// 713. Subarray Product Less Than K
+// Medium
+
+// Share
+// Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+ 
+
+// Example 1:
+
+// Input: nums = [10,5,2,6], k = 100
+// Output: 8
+// Explanation: The 8 subarrays that have product less than 100 are:
+// [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+// Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+// Example 2:
+
+// Input: nums = [1,2,3], k = 0
+// Output: 0
+
+
+
+
+var numSubarrayProductLessThanK = function (nums, k) {
+  let product = 1;
+  let l = 0;
+  let r = 0;
+  let count = 0;
+
+  while (l < nums.length && r < nums.length) {
+    if (product * nums[r] < k) {
+      product *= nums[r];
+      count += r - l + 1;
+      r++;
+    } else {
+      product /= nums[l];
+      l++;
+    }
+  }
+
+  return count;
+};
+
+//Okay this is interesting
+//so basically, we have an array of unsorted indexes.
+//we need to split the array into all subarrays
+//we have to make the product of each subarray less than k
+
+//in the examples, it's less than a 100
+
+//now what's the approach to solve this problem?
+// Two Pointers
+// Since the multiplication will not overflow, we can use one variable to store the product
+// We multiply the current number and adjust the left pointer until we have product smaller than k
+// Then the number of product less than k is equal to current - left + 1
+// Time complexity O(n)
+// Space complexity O(1)
