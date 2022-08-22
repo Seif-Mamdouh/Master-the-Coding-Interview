d3306722540db1f7477b20278c53ba2bcead0ebc
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-08-22 10:45:17 -0400

    day42

--- DS and Algo/CyclicSort/Problem Challenge 3: Find the First K Missing Positive Numbers (hard).js
+++ DS and Algo/CyclicSort/Problem Challenge 3: Find the First K Missing Positive Numbers (hard).js
@@ -31,6 +31,41 @@ function find_first_k_missing_positive(nums, k) {
 
     let i = 0;
     let n = nums.length;
+    
+    //sort the array using cyclic sort
+    while (i < n) {
+        const j = nums[i] - 1;
+        if (nums[i] > 0 && nums[i] <= n && nums[i] !== nums[j]) {
+          [nums[i], nums[j]] = [nums[j], nums[i]];
+        } else {
+          i += 1;
+        }
+
+        console.log(nums);
+    }
+
+  missingNumbers = [];
+  const extraNumbers = new Set();
+  for (i = 0; i < n; i++) {
+    if (missingNumbers.length < k) {
+      if (nums[i] !== i + 1) {
+        missingNumbers.push(i + 1);
+        extraNumbers.add(nums[i]);
+      }
+    }
+  }
+
+  // add the remaining missing numbers
+  i = 1;
+  while (missingNumbers.length < k) {
+    const candidateNumber = i + n;
+    // ignore if the array contains the candidate number
+    if (!extraNumbers.has(candidateNumber)) {
+      missingNumbers.push(candidateNumber);
+    }
+    i += 1;
+  }
+  return missingNumbers;
 
 }
 
