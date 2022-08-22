ff8ec9675148dfe417a9c1c783ac1538abda89a7
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-08-22 12:43:44 -0400

    day42

---
+++ DS and Algo/Islands (Matrix Traversal)/200. Number of Islands.js
@@ -0,0 +1,90 @@
+/*
+200. Number of Islands
+Medium
+
+15772
+
+367
+
+Add to List
+
+Share
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+Matrix: 
+
+0, 1, 1, 1, 0
+0, 0, 0, 1, 1
+0, 1, 1, 1, 0
+0, 1, 1, 0, 0
+0, 0, 0, 0, 0
+
+1, 1, 1, 0, 0
+0, 1, 0, 0, 1
+0, 0, 1, 1, 0
+0, 0, 1, 0, 0
+0, 0, 1, 0, 0
+
+
+answer = 3
+
+*/
+
+
+
+
+console.log(
+  count_islands_BFS([
+    [
+      ["1", "1", "1", "1", "0"],
+      ["1", "1", "0", "1", "0"],
+      ["1", "1", "0", "0", "0"],
+      ["0", "0", "0", "0", "0"],
+    ],
+  ])
+);
+
+console.log(
+  count_islands_DFS([
+    [
+      ["1", "1", "1", "1", "0"],
+      ["1", "1", "0", "1", "0"],
+      ["1", "1", "0", "0", "0"],
+      ["0", "0", "0", "0", "0"],
+    ],
+  ])
+);
+
+
+// console.log(
+//   count_islands_DFS([
+//     [1, 1, 1, 0, 0],
+//     [0, 1, 0, 0, 1],
+//     [0, 0, 1, 1, 0],
+//     [0, 0, 1, 0, 0],
+//     [0, 0, 1, 0, 0],
+//   ])
+// );
