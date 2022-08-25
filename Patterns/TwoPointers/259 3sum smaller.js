99875296548ceaf78b059ef7323acbc447118678
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-08-25 16:32:54 -0400

    day45

--- Patterns/TwoPointers/259 3sum smaller.js
+++ Patterns/TwoPointers/259 3sum smaller.js
@@ -26,36 +26,76 @@ Input: nums = [0], target = 0
 Output: 0
 */
 
-function triplet_with_smaller_sum(arr, target) {
-  arr.sort((a, b) => a - b);
-  let count = 0;
-  for (i = 0; i < arr.length - 2; i++) {
-    count += search_pair(arr, target - arr[i], i);
-  }
-  return count;
-}
+// function triplet_with_smaller_sum(arr, target) {
+//     arr.sort((a, b) => a - b);
+//     let count = 0;
+//     for (let i = 0; i < arr.length - 2; i++) {
+//         count += search_pair(arr, target - arr[i], i);
+//     }
 
-function search_pair(arr, target_sum, first) {
-  let count = 0;
-  let left = first + 1,
-    right = arr.length - 1;
-  while (left < right) {
-    if (arr[left] + arr[right] < target_sum) {
-      // found the triplet
-      // since arr[right] >= arr[left], therefore, we can replace arr[right] by any
-      // number between left and right to get a sum less than the target sum
-      count += right - left;
-      left += 1;
-    } else {
-      right -= 1; // we need a pair with a smaller sum
-    }
-  }
-  return count;
-}
+//     return count;
+
+// }
+
+// function search_pair(arr, target_sum, first) {
+//     let count = 0;
+//     let left = first + 1;
+//     right = arr.length - 1;
+//     if (arr[left] + arr[right] < target_sum) {
+//         //found the triplet
+//         //since arr[right] is grerater than the left one
+//         //this means that any number between left and right is less than target sum
+//         count += right - left;
+//         left += 1;
+//     } else {
+//         right += 1;
+//     }
+
+//     return count;
+// }
 
 
 // At this stage, our problem translates into finding a pair whose sum is less than 
-// “target - Xtarget − X” (as from the above equation Y + Z == target - XY + Z == target−X).
+// “target - Xtarget − X” (as from the above equation Y + Z == target - X 
+//Y + Z == target−X).
+
+// console.log(triplet_with_smaller_sum([-1, 0, 2, 3], 3));
+// console.log(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5));
+
+
+//Problem: 
+// Write a function to return the list of all such triplets instead of the count.
+
+function triplet_with_smaller_sum(arr, target) {
+  arr.sort((a, b) => a - b);
+    let triplets = [];
+    for (let i = 0; i < arr.length - 2; i++) {
+        search_pair(arr, target - arr[i], i, triplets);
+  }
+
+  return triplets;
+}
+
+function search_pair(arr, target_sum, first, triplets) {
+    let left = first + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        if (arr[left] + arr[right] < target_sum) {
+          // found the triplet
+          // since arr[right] >= arr[left], therefore, we can replace arr[right] by any
+          // number between left and right to get a sum less than the target sum
+                  for (i = right; i > left; i--) {
+                    triplets.push([arr[first], arr[left], arr[i]]);
+                  }
+            left += 1;
+        } else {
+            right -= 1;
+        }
+    }
+}
+
+
 
 console.log(triplet_with_smaller_sum([-1, 0, 2, 3], 3));
 // console.log(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5));
