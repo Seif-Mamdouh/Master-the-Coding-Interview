ed1a1df6866b5571c43372193b48537045d9a36d
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-08-25 08:36:13 -0400

    day44

--- Patterns/TwoPointers/Pair with Target Sum.js
+++ Patterns/TwoPointers/Pair with Target Sum.js
@@ -22,7 +22,9 @@ function pair_with_target_sum(arr, targetSum) {
   const nums = {}; // to store numbers and their indices
   for (let i = 0; i < arr.length; i++) {
     const num = arr[i];
-    if (targetSum - num in nums) {
+    // console.log(num);
+    if (targetSum + num in nums) {
+      console.log(num);
       return [nums[targetSum - num], i];
     }
     nums[arr[i]] = i;
@@ -31,4 +33,4 @@ function pair_with_target_sum(arr, targetSum) {
 }
 
 console.log(pair_with_target_sum([1, 2, 3, 4, 6], 6));
-console.log(pair_with_target_sum([2, 5, 9, 11], 11));
+// console.log(pair_with_target_sum([2, 5, 9, 11], 11));
