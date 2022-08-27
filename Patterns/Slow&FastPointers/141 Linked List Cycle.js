25ba36f76da33d87171fee3df55460afd08b1def
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-08-27 13:33:50 -0400

    day47

---
+++ Patterns/Slow&FastPointers/141 Linked List Cycle.js
@@ -0,0 +1,79 @@
+// 141. Linked List Cycle
+// Easy
+
+// 9609
+
+// 861
+
+// Add to List
+
+// Share
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+// Example 2:
+
+
+// Input: head = [1,2], pos = 0
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+
+//answer 
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+function find_cycle_length(head) {
+  let slow = head,
+    fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (slow === fast) {
+      // found the cycle
+      return calculate_cycle_length(slow);
+    }
+  }
+  return 0;
+}
+
+function calculate_cycle_length(slow) {
+  let current = slow,
+    cycle_length = 0;
+  while (true) {
+    current = current.next;
+    cycle_length += 1;
+    if (current === slow) {
+      break;
+    }
+  }
+  return cycle_length;
+}
+
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+head.next.next.next.next.next.next = head.next.next;
+console.log(`LinkedList cycle length: ${find_cycle_length(head)}`);
+
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`LinkedList cycle length: ${find_cycle_length(head)}`);
