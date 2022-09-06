60dbde6dc5388526fdb8ee1d912f1562ad50163a
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-09-06 14:42:29 -0400

    day54 First day at RU2022

--- Patterns/Tree DFS/112 Path Sum.js
+++ Patterns/Tree DFS/112 Path Sum.js
@@ -37,6 +37,32 @@ Output: false
 Explanation: Since the tree is empty, there are no root-to-leaf paths.
 */
 
+/*---------------------------ANSWER-------------------------------------
+ To recursively traverse a binary tree in a DFS fashion, we can start from the root and at every step, 
+make two recursive calls one for the left and one for the right child.
+
+Here are the steps for our Binary Tree Path Sum problem:
+
+1 - Start DFS with the root of the tree.
+
+2 - If the current node is not a leaf node, do two things:
+      Subtract the value of the current node from the given number to get a new sum => S = S - node.value
+      Make two recursive calls for both the children of the current node with the new number calculated in the previous step.
+
+3 - At every step, see if the current node being visited is a leaf node and if its value is equal 
+    to the given number ‘S’. If both these conditions are true, we have found the required root-to-leaf path, therefore return true.
+4 - If the current node is a leaf but its value is not equal to the given number ‘S’, return false.
+
+
+
+
+
+
+*/
+
+
+
+
 class TreeNode {
   constructor(val, left = null, right = null) {
     this.val = val;
@@ -45,28 +71,6 @@ class TreeNode {
   }
 }
 
-function hasPath(root, sum) {
-  if (root === null) {
-    return false;
-  }
-
-  // if the current node is a leaf and its value is equal to the sum, we've found a path
-  if (root.val === sum && root.left === null && root.right === null) {
-    return true;
-  }
-
-  // recursively call to traverse the left and right sub-tree
-  // return true if any of the two recursive call return true
-  return (
-    hasPath(root.left, sum - root.val) || hasPath(root.right, sum - root.val)
-  );
+function hasPath(root, sum ) {
+  
 }
-
-const root = new TreeNode(12);
-root.left = new TreeNode(7);
-root.right = new TreeNode(1);
-root.left.left = new TreeNode(9);
-root.right.left = new TreeNode(10);
-root.right.right = new TreeNode(5);
-console.log(`Tree has path: ${hasPath(root, 23)}`);
-console.log(`Tree has path: ${hasPath(root, 16)}`);
