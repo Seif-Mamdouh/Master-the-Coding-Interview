bf828efa0334f0ddeb2eecaa9a62724b91a836d6
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-09-24 11:52:49 -0400

    day 60

---
+++ Patterns/Bitwise XOR/137.js
@@ -0,0 +1,51 @@
+// 137. Single Number II
+
+// Medium
+
+// 4695
+
+// 509
+
+// Add to List
+
+// Share
+// Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,2,3,2]
+// Output: 3
+// Example 2:
+
+// Input: nums = [0,1,0,1,0,1,99]
+// Output: 99
+
+
+137. Single Number II
+Medium
+
+4695
+
+509
+
+Add to List
+
+Share
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
