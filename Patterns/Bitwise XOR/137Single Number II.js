bcfafe4b61d8a967cc66a53e74906408dbf175b9
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-09-24 12:06:45 -0400

    lol Fuck XOR I started using Hashmaps instead lol

--- Patterns/Bitwise XOR/137Single Number II.js
+++ Patterns/Bitwise XOR/137Single Number II.js
@@ -25,27 +25,22 @@
 // Output: 99
 
 
-137. Single Number II
-Medium
+var singleNumber = function (nums) {
+  var hash = {};
 
-4695
+  for (var i = 0; i < nums.length; i++) {
+    var key = nums[i];
 
-509
+    if (!hash[key]) {
+      hash[key] = 1;
+    } else if (hash[key] == 1) {
+      hash[key]++;
+    } else if (hash[key] == 2) {
+      delete hash[key];
+    }
+  }
 
-Add to List
-
-Share
-Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
-
-You must implement a solution with a linear runtime complexity and use only constant extra space.
-
- 
-
-Example 1:
-
-Input: nums = [2,2,3,2]
-Output: 3
-Example 2:
-
-Input: nums = [0,1,0,1,0,1,99]
-Output: 99
+  for (var k in hash) {
+    return k;
+  }
+};
