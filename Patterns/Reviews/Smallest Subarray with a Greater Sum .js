b02e3ec2456b792e38c83553f6e0835ca68833f9
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-09-27 21:35:20 -0400

    review sliding window

---
+++ Patterns/Reviews/Smallest Subarray with a Greater Sum .js
@@ -0,0 +1,20 @@
+function smallest_subarray_sum(s, arr) {
+    let windowSum = 0;
+    let windowStart = 0; 
+    let minLength = Infinity;
+
+
+    for (let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+        
+    }
+}
+
+console.log(
+  `Smallest subarray length: ` + smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2])
+);
+console.log(
+  `Smallest subarray length: ` + smallest_subarray_sum(7, [2, 1, 5, 2, 8])
+);
+console.log(
+  `Smallest subarray length: ` + smallest_subarray_sum(8, [3, 4, 1, 1, 6])
+);
