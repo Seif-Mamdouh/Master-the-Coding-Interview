da84e55faf4bba22306d0dc4af1b3bd5aa8775e5
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-11-07 21:39:43 -0500

    Epic-React

--- EpicReact/ReactFund/06.js
+++ EpicReact/ReactFund/06.js
@@ -133,3 +133,45 @@ export default App
 
 
 //03 Extra Credit
+
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({ onSubmitUsername }) {
+  //we created a state hook, error and setError
+  const [username, setUsername] = React.useState('')
+  // 🐨 add a submit event handler here (`handleSubmit`).
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.dir(event);
+    // console.log(event)
+    onSubmitUsername(username);
+  }
+ 
+  function handleChange(event) {
+    setUsername(event.target.value.toLowerCase());
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput"
+          type="text"
+          onChange={handleChange}
+          value={username}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
