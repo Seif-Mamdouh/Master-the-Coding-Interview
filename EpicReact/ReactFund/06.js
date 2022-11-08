1a55b23f45b22a1eead0e07d1785d1e6a89f862f
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-11-07 21:32:29 -0500

    Epic React

--- EpicReact/ReactFund/06.js
+++ EpicReact/ReactFund/06.js
@@ -1,3 +1,46 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  // 🐨 add a submit event handler here (`handleSubmit`).
+  // 💰 Make sure to accept the `event` as an argument and call
+  // `event.preventDefault()` to prevent the default behavior of form submit
+  // events (which refreshes the page).
+  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+  //
+  // 🐨 get the value from the username input (using whichever method
+  // you prefer from the options mentioned in the instructions)
+  // 💰 For example: event.target.elements[0].value
+  // 🐨 Call `onSubmitUsername` with the value of the input
+
+  // 🐨 add the onSubmit handler to the <form> below
+
+  // 🐨 make sure to associate the label to the input.
+  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  return (
+    <form>
+      <div>
+        <label>Username:</label>
+        <input type="text" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
+
+
+
+
+
 import * as React from "react";
 
 function UsernameForm(onSubmitUsername) {
@@ -6,6 +49,9 @@ function UsernameForm(onSubmitUsername) {
     onSubmitUsername(event.target.elements.usernameInput.value);
   }
 
+
+  // 🐨 make sure to associate the label to the input.
+  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -36,7 +82,52 @@ export default App;
 
 
 // 02 extra Credit
+// Dynamic Forms
+// 💯 Validate lower-case
+// http://localhost:3000/isolated/final/06.extra-2.js
 
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const [error, setError] = React.useState(null)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    onSubmitUsername(event.target.elements.usernameInput.value)
+  }
+
+  function handleChange(event) {
+    const {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    setError(isLowerCase ? null : 'Username must be lower case')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" type="text" onChange={handleChange} />
+      </div>
+      <div role="alert" style={{color: 'red'}}>
+        {error}
+      </div>
+      <button disabled={Boolean(error)} type="submit">
+        Submit
+      </button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return (
+    <div style={{minWidth: 400}}>
+      <UsernameForm onSubmitUsername={onSubmitUsername} />
+    </div>
+  )
+}
+
+export default App
 
 
 
