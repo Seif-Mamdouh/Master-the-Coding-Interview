9528c3505e7352b33c07c9c6c3ea478882aa11c9
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-11-13 14:59:42 -0500

    SQL

--- Patterns/SQL/.sql
+++ Patterns/SQL/.sql
@@ -1,25 +1,32 @@
-1667. Fix Names in a Table
+1484. Group Sold Products By The Date
 
 Companies
 SQL Schema
-Table: Users
+Table Activities:
 
-+----------------+---------+
-| Column Name    | Type    |
-+----------------+---------+
-| user_id        | int     |
-| name           | varchar |
-+----------------+---------+
-user_id is the primary key for this table.
-This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
  
 
-Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+Write an SQL query to find for each date the number of different products sold and their names.
 
-Return the result table ordered by user_id.
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
 
 The query result format is in the following example.
 
 
-select user_id , CONCAT( UPPER( SUBSTR(name,1,1) ) , LOWER( SUBSTR(name,2) ) ) as name
-    from Users order by user_id ASC;
+SELECT  
+	sell_date,
+	(COUNT(sell_date ) ) as num_sold ,
+	GROUP_CONCAT(distinct product  ORDER BY product) as products 
+FROM 
+	(SELECT DISTINCT sell_date,product FROM Activities) as Activities
+GROUP BY sell_date;
