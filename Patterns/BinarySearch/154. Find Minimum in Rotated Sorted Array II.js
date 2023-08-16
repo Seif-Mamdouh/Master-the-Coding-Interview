488cfaa38ee4f042909536b27b857c1629239f51
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-08-16 13:18:21 -0400

    rortated

---
+++ Patterns/BinarySearch/154. Find Minimum in Rotated Sorted Array II.js
@@ -0,0 +1,19 @@
+ar findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left < right){
+        let mid = Math.floor((left+right)/2);
+
+        if(nums[mid] > nums[right]){
+            left = mid + 1;
+        } else if (nums[mid] < nums[right]) {
+            right = mid;
+        } else {
+            // Handle duplicates by decrementing 'right'
+            right--;
+        }
+    }
+
+    return nums[left];
+};
