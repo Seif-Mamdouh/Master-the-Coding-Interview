c9956c0d55102f7ad3f8ad49b312b6ea6f8c0fee
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-02-05 15:04:18 -0500

    adjancey list outputs for directed and undirected

--- DataStructures/Graphs/AdjacenyList.js
+++ DataStructures/Graphs/AdjacenyList.js
@@ -13,7 +13,7 @@ class Graph {
   //addEdge
   addEdge(node1, node2) {
     this.adjanceyList.get(node1).push(node2);
-    this.adjanceyList.get(node2).push(node1);
+    // this.adjanceyList.get(node2).push(node1);
   }
 
   //has edge
@@ -26,3 +26,67 @@ class Graph {
     return this.adjanceyList.get(node);
   }
 }
+
+const graph = new Graph();
+
+graph.addNode(1);
+graph.addNode(2);
+graph.addNode(3);
+graph.addNode(4);
+graph.addNode(5);
+graph.addNode(6);
+
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(2, 4);
+graph.addEdge(3, 5);
+graph.addEdge(4, 6);
+graph.addEdge(5, 6);
+
+
+function printGraph(graph) {
+  // for (let [key, value] of graph.adjanceyList) {
+  //   console.log(key, value);
+  // }
+
+  for (let [node, neighbors] of graph.adjanceyList) {
+    console.log("adjancey list " + JSON.stringify(neighbors))
+    console.log(node, "=>", neighbors.join(", "));
+  }
+}
+
+printGraph(graph);
+
+
+// Output
+
+
+// Undirected Graph
+// adjancey list [2,3]
+// 1 => 2, 3
+// adjancey list [1,4]
+// 2 => 1, 4
+// adjancey list [1,5]
+// 3 => 1, 5
+// adjancey list [2,6]
+// 4 => 2, 6
+// adjancey list [3,6]
+// 5 => 3, 6
+// adjancey list [4,5]
+// 6 => 4, 5
+
+
+
+// Directed Graph
+// adjancey list [2,3]
+// 1 => 2, 3
+// adjancey list [4]
+// 2 => 4
+// adjancey list [5]
+// 3 => 5
+// adjancey list [6]
+// 4 => 6
+// adjancey list [6]
+// 5 => 6
+// adjancey list []
+// 6 => 
