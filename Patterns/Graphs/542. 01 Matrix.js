dabe0bac59301eb410c4ff49543a53bb9eee2364
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-02-06 13:21:37 -0500

    Rotten Oragnges

---
+++ Patterns/Graphs/542. 01 Matrix.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function (mat) {
+  let rows = mat.length;
+  let cols = mat[0].length;
+  let queue = [];
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (mat[r][c] === 0) {
+        queue.push([r, c]);
+      } else {
+        mat[r][c] = "#";
+      }
+    }
+  }
+
+  while (queue.length > 0) {
+    const [row, column] = queue.shift();
+    const directions = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ];
+
+    for (const [dr, dc] of directions) {
+      const newRow = row + dr;
+      const newCol = column + dc;
+
+      if (
+        newRow >= 0 &&
+        newRow < rows &&
+        newCol >= 0 &&
+        newCol < cols &&
+        mat[newRow][newCol] === "#"
+      ) {
+        mat[newRow][newCol] = mat[row][column] + 1;
+        queue.push([newRow, newCol]);
+      }
+    }
+  }
+
+  return mat;
+};
