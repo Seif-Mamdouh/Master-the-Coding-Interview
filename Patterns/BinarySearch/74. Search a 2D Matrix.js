a243b75416d61b44d6ac83312c26f4018010b6e1
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-02-16 14:24:04 -0500

    valtine day solution

--- Patterns/BinarySearch/74. Search a 2D Matrix.js
+++ Patterns/BinarySearch/74. Search a 2D Matrix.js
@@ -1,3 +1,39 @@
+// V Day solution
+
+var searchMatrix = function (matrix, target) {
+  function binary_search(row, target) {
+    let left = 0;
+    let right = row.length - 1;
+
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+
+      if (row[mid] === target) {
+        return true;
+      } else if (row[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return false;
+  }
+
+  let row = matrix.length;
+
+  for (let i = 0; i < row; i++) {
+    if (binary_search(matrix[i], target)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+
+
+//Aug 2023 Solution
 var searchMatrix = function (matrix, target) {
   function searchBinary(matrix, target, rowIndex) {
     let left = 0;
