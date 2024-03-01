a2004f184b2e6918fe5469ba4f6863f487786d8a
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-03-01 11:46:34 -0500

    merged list

---
+++ Patterns/LinkedList/21. Merge Two Sorted Lists.js
@@ -0,0 +1,19 @@
+var mergeTwoLists = function (list1, list2) {
+  let dummy = new ListNode();
+  let tail = dummy;
+
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      tail.next = list2;
+      list2 = list2.next;
+    } else {
+      tail.next = list1;
+      list1 = list1.next;
+    }
+    tail = tail.next;
+  }
+
+  tail.next = list1 || list2;
+
+  return dummy.next;
+};
