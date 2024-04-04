96bfe4bbfc1946db2ee534e49f2a82b60f42f442
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-04-04 16:41:25 -0400

    stacksss

---
+++ Patterns/Stacks&Quesses/150. Evaluate Reverse Polish Notation.js
@@ -0,0 +1,32 @@
+var evalRPN = function (tokens) {
+  let stack = [];
+
+  for (token of tokens.slice()) {
+    if (!isNaN(parseInt(token))) {
+      stack.push(parseInt(token));
+    }
+    if (token === "+") {
+      let value2 = stack.pop();
+      let value1 = stack.pop();
+      stack.push(value1 + value2);
+    } else if (token === "*") {
+      let value2 = stack.pop();
+      let value1 = stack.pop();
+      stack.push(value1 * value2);
+    } else if (token === "/") {
+      let operand2 = stack.pop();
+      let operand1 = stack.pop();
+      stack.push(
+        operand1 / operand2 > 0
+          ? Math.floor(operand1 / operand2)
+          : Math.ceil(operand1 / operand2)
+      );
+    } else if (token === "-") {
+      let value2 = stack.pop();
+      let value1 = stack.pop();
+      stack.push(value1 - value2);
+    }
+  }
+
+  return stack.pop();
+};
