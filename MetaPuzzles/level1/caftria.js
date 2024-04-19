64193a42f0e015617ceef2f57f749ad0f944a01b
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-04-19 11:52:53 -0700

    more humbling

--- MetaPuzzles/level1/caftria.js
+++ MetaPuzzles/level1/caftria.js
@@ -1,29 +1,29 @@
+// N seats
+// K social distancing
+// M diners
 function getMaxAdditionalDinersCount(N, K, M, S) {
+  S.sort((a, b) => a - b);
+  let i = 0,
+    count = 0,
+    start = 1,
+    end = 1;
 
-    let sorted = S.sort((a, b) => a - b);
-  
-    let personSeated = 0;
-    let start = 1;
-    let range = 0;
-  
-    for (seatedDiner of sorted) {
-        range = seatedDiner - start;
-        personSeated += Math.floor(range / (K + 1));
-        start = seatedDiner + K + 1;
-        console.log("start", start)
-    }
+  // Number of people who can sit before the first
+  count += Math.max(Math.ceil((S[0] - K - 1) / (K + 1)), 0);
+
+  console.log(Math.ceil((S[0] - K - 1) / (K + 1)), 0);
 }
 
+getMaxAdditionalDinersCount(10, 1, 2, [6, 2]);
 
-    // for(long seatedDiner : S){
-    //   range = seatedDiner - start;
-    //   guests += (long)(Math.floor(range/(K + 1)));
-    //   start = seatedDiner + K + 1;
-    // }
-    // range = N - start + 1;
-    // guests += Math.ceil(range/ (K+1));
-    
+// // Number of people who can sit before the first            and after the last person already sitting
+// count += Math.max(Math.ceil((S[0] - K - 1) / (K + 1)), 0) + Math.max(Math.ceil((N - (S[M - 1] + K + 1) + 1) / (K + 1)), 0)
 
-    // return guests;
+// // Calculate for all the allowed spaces between people
+// while (i < M - 1) {
+//     start = S[i] + K + 1
+//     end = S[++i] - K - 1
+//     count += Math.ceil((end - start + 1) / (K + 1))
+// }
 
-getMaxAdditionalDinersCount(10, 1, 2, [2, 6])
+// return count;
