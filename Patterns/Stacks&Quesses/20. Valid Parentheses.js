2a4c12c5a96eb84acd7a05c96850965cad7d1acb
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-05-25 09:45:52 -0400

    MDW

--- Patterns/Stacks&Quesses/20. Valid Parentheses.js
+++ Patterns/Stacks&Quesses/20. Valid Parentheses.js
@@ -1,22 +1,35 @@
-var isValid = function (s) {
-  let pairs = {
-    "(": ")",
-    "{": "}",
-    "[": "]",
-  };
+// var isValid = function (s) {
+//   let pairs = {
+//     "(": ")",
+//     "{": "}",
+//     "[": "]",
+//   };
     
-  let stack = [];
+//   let stack = [];
 
-  for (let char of s) {
-    if (char === "(" || char === "{" || char === "[") {
-      stack.push(char);
+//   for (let char of s) {
+//     if (char === "(" || char === "{" || char === "[") {
+//       stack.push(char);
+//     } else {
+//       const top = stack.pop();
+//       if (!top || pairs[top] !== char) {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return stack.length === 0;
+// };
+
+
+const logEvenOrOdd = (x) => {
+  for (let t = 1; t < x + 1; t++) {
+    if (t % 2 === 0) {
+      console.log("Even");
     } else {
-      const top = stack.pop();
-      if (!top || pairs[top] !== char) {
-        return false;
-      }
+      console.log("Odd");
     }
   }
-
-  return stack.length === 0;
 };
+
+logEvenOrOdd(10);
