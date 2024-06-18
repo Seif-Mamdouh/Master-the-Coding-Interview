c4dc6c56689d5532bf2ce7413b79e8cd128d2a06
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-06-17 22:23:01 -0400

    this is just rough

---
+++ Patterns/Graphs/310MinimumHeightTrees.js
@@ -0,0 +1,13 @@
+var findMinHeightTrees = function (n, edges) {
+
+    const adjlist = Array.from({ length: n }, () => []);
+    for(let [n1, n2] of edges){
+        adjlist[n1].push(n2);
+        adjlist[n2].push(n1);
+    }
+
+
+    
+};
+
+findMinHeightTrees(4, [[1,0],[1,2],[1,3]]);
