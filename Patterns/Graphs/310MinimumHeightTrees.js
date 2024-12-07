2ffd88930f0820f9fcbc71bf34a25ea61b0f0e88
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-12-06 21:56:54 -0500

    refactor findMinHeightTrees function to streamline code

--- Patterns/Graphs/310MinimumHeightTrees.js
+++ Patterns/Graphs/310MinimumHeightTrees.js
@@ -1,13 +1,11 @@
+// O(V + E) Time | O(V) Space
 var findMinHeightTrees = function (n, edges) {
 
     const adjlist = Array.from({ length: n }, () => []);
     for(let [n1, n2] of edges){
         adjlist[n1].push(n2);
         adjlist[n2].push(n1);
-    }
-
-
-    
+    }    
 };
 
 findMinHeightTrees(4, [[1,0],[1,2],[1,3]]);
